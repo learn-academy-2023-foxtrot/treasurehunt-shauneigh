3389cf8b3e37a4211105f175877fe6577bef1685
shaunfitzgarald <shaunfitzgarald@gmail.com>
2023-09-18 10:27:36 -0700

    Worked on the grid

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,20 @@
 import React from "react"
 
-const Square = () => {
+const Square = ({board}) => {
+
+  //destructure a specific property
+//{board} - referencing the name assigned to the prop passed from App.js
+console.log("board", board)
+
   return (
     <>
-      <div className="square"></div>
+      (board.map((square, index) => {
+        return(
+          <div className="square" key={index}>
+            {square}
+          </div>
+        )
+      })}
     </>
   )
 }
